5acd3c92acbd9605f0f28454bb55227581229ec4
a.kurmanov <a.kurmanov@jgarant.kz>
2024-12-23 15:14:51 +0500

    Алгоритмы до связанного списка

--- src/main/java/kz/jgarant/dit/simpleexample1/Application.java
+++ src/main/java/kz/jgarant/dit/simpleexample1/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleExample1Application {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleExample1Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
